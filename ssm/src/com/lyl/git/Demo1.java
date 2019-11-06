291866d542c4d2f2a82b9fe406753d21be1d68d8
leiyuanlin <1524252532@qq.com>
2019-11-06 21:39:57 +0800

    本地仓库接受

---
+++ ssm/src/com/lyl/git/Demo1.java
@@ -0,0 +1,13 @@
+package com.lyl.git;
+
+/**
+ * @authorlyl
+ * @site
+ * @company
+ * @create  2019-11-06 21:35
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("idea git demo");
+    }
+}
